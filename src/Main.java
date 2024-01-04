689f7e58d4f3dac5fb8c113406ec70ad0b9b8cf1
Alina <alina150120051@gmail.com>
2024-01-04 21:40:02 +0300

    i

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,30 @@ import other.Thing;
 public class Main {
     public static void main(String[] args) {
         Human alice = new Human("Алиса");
+        Human solider1 = new Human("Первый солдат");
+        Human solider2 = new Human("Второй солдат");
+        Human jack = new Human("Червонный Валет");
+        Human rabbit = new Human("Белый Кролик");
+        Human judge = new Human("Судья");
+        Thing throne = new Thing("судейский трон");
+        Thing chains = new Thing("цепи");
+        Thing pipe = new Thing("трубка");
+        Thing scroll = new Thing("пергаментный свиток");
         Thing court = new Thing("суд");
         Thing judgment = new Thing("судебный зал");
-        Thing pie = new Thing("блюдо с пирожками");
+        Thing dish = new Thing("блюдо");
+        Thing pies = new Thing("пирожки");
         Thing table = new Thing("стол");
         Thing spit = new Thing("слюнки");
         Thing surrounding = new Thing("окружение");
         Thing everything = new Thing("все");
 
+
+
         table.doing(Moves.STAND, judgment, false);
-        pie.doing(Moves.STAND_BEAUTIFUL, table, false);
-        pie.addStatus("имеет аппетитный вид");
-        pie.printStatus();
+        dish.doing(Moves.STAND_BEAUTIFUL, table, false);
+        pies.addStatus("имеют аппетитный вид");
+        pies.printStatus();
         spit.doing(Moves.LEAK, alice, false);
         alice.want(Moves.END, court);
         alice.want(Moves.INVITE, table);
--- src/interfaces/Want.java
+++ src/interfaces/Want.java
@@ -3,5 +3,8 @@ package interfaces;
 import other.*;
 
 public interface Want {
-    public void want(Moves move, Thing smth);
+    public void want_to_do(Moves move);
+    public void want_to_do_with(Moves move, Thing smth);
+    public void want_smth_to_do(Thing smth1, Moves move);
+    public void want_smth_to_do_with(Thing smth1, Moves move, Thing smth);
 }
